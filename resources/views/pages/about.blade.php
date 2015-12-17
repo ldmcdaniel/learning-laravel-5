c5f7ebc9628e3b6957a18d58ee753f68a3c1163b
Lucas McDaniel <lucas.mcdaniel@bernardhealth.com>
2015-12-17 14:22:46 -0600

    initial commit

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+
+    <h1>About Me {{ $first }} {{ $last }}</h1>
+    <p> This is the aobu tme page that just has nonesene teststs.</p>
+
+</body>
+</html>
